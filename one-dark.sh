d953883e2257df729680e4b0a57f5840edee7396
Matthew Promenchenkel <mattprom@gmail.com>
2018-03-06 14:31:38 -0500

    fixed background color to match vim theme

--- one-dark.sh
+++ one-dark.sh
@@ -67,7 +67,7 @@ if which "$DCONF" > /dev/null 2>&1; then
         # update profile values with theme options
         dset visible-name "'$PROFILE_NAME'"
         dset palette "['#000000', '#E06C75', '#98c379', '#d19a66', '#61aeee', '#c678dd', '#56b6c2', '#abb2bf', '#5c6370', '#e06c75', '#98c379', '#d19a66', '#62afee', '#c678dd', '#56b6c2', '#ffffff']"
-        dset background-color "'#282c34'"
+        dset background-color "'#262626'"
         dset foreground-color "'#abb2bf'"
         dset bold-color "'#ABB2BF'"
         dset bold-color-same-as-fg "true"
