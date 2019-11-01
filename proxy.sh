2662e61b1a73fbdee3c58d08ad7cb5f0b53bf9fa
Matthew Promenchenkel <mattprom@gmail.com>
2019-11-01 16:32:03 -0400

    update osx bashrc add proxy script

--- proxy.sh
+++ proxy.sh
@@ -3,12 +3,12 @@
 # This probably already exists and can probably be handled better, but I say that lot
 #ps aux | grep -q "[s]sh -D 8123 -f -C -q -N user@host"
 
-if `ps aux | grep -q "[s]sh -D 8123 -f -C -q -N"` ; then 
+if `ps aux | grep -q "[s]sh -D 8123 -f -C -N"` ; then 
   echo "Proxy already running" 
-  ps aux | grep "[s]sh -D 8123 -f -C -q -N"
+  ps aux | grep "[s]sh -D 8123 -f -C -N"
 else
   echo "Starting new proxy session"
-  ssh -D 8123 -f -C -q -N login
-  ps aux | grep "[s]sh -D 8123 -f -C -q -N"
+  ssh -D 8123 -f -C -N merit-login03
+  ps aux | grep "[s]sh -D 8123 -f -C -N"
 fi
 
